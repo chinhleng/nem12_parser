9ab73dc5dba21c361b6db8f284527502ccf33e21
chinhleng <127060966+chinhleng@users.noreply.github.com>
2024-03-08 17:27:50 +0700

    readFile and get NMIs

--- nem12_parser/nem12_parser_lib.fs
+++ nem12_parser/nem12_parser_lib.fs
@@ -5,4 +5,13 @@ module nem12 =
     let readFile filePath =
         File.ReadAllLines filePath
         
-    
+    let getNMIs (fileContent:string array) =
+        let NMIs = ResizeArray<string>()
+        for line in fileContent do
+            let lineContent = line.Split(",")
+            if lineContent.Length >= 2 then
+                if lineContent[0] ="200" then
+                    NMIs.Add lineContent[1]
+        NMIs
+        |> Seq.distinct
+        |> Array.ofSeq
